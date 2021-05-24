cbc8f113c587da289428613b50f796af89cde4c7
jerome <jeromebrown97@gmail.com>
2021-05-24 10:27:55 -0500

    added chicken bowl and brown rice line

--- Git demo/Program.cs
+++ Git demo/Program.cs
@@ -9,6 +9,7 @@ namespace Git_demo
             Console.WriteLine("Hello World!");
             Console.WriteLine("I had coffee for breakfast");
             Console.WriteLine("I would like to have chipotle for lunch");
+            Console.WriteLine("I like chicken bowls with brown rice");
         }
     }
 }
