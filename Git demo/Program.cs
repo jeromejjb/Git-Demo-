7f15659ae414a74fc2c97a81b15d2744bb84279c
jerome <jeromebrown97@gmail.com>
2021-05-24 10:17:49 -0500

    Git demo

---
+++ Git demo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Git_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I had coffee for breakfast");
+            Console.WriteLine("I would like to have chipotle for lunch");
+        }
+    }
+}
